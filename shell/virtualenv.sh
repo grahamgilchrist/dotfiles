c70af19d2987d0b7733f01ff70789760677a10bf
Graham Gilchrist <git@grahamgilchrist.com>
2015-05-02 03:54:36 +0100

    Test for virtualenvwrapper before generating an error

--- shell/virtualenv.sh
+++ shell/virtualenv.sh
@@ -1,7 +1,10 @@
 # virtualenvwrapper
 export WORKON_HOME=~/.virtualenvs
 export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
-source /usr/local/bin/virtualenvwrapper_lazy.sh
+
+if [ -f "/usr/local/bin/virtualenvwrapper_lazy.sh" ]; then
+    source /usr/local/bin/virtualenvwrapper_lazy.sh
+fi
 
 alias mkvenv2='mkvirtualenv `basename \`pwd\`` -a `pwd` -p `which python2.7`'
 alias mkvenv3='mkvirtualenv `basename \`pwd\`` -a `pwd` -p `which python3`'
