a0e014687cf75fe8f4b3e68c6473271fd5b849eb
Graham Gilchrist <git@grahamgilchrist.com>
2015-05-04 11:30:15 +0100

    Add comments when setting up bash config

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,11 +14,17 @@ ln -s ~/.dotfiles/.gitignore ~/.gitignore
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # Most unix systems use a .bashrc for user config
-if [ -f "~/.bashrc" ]; then
+if [ -f "$HOME/.bashrc" ]; then
+    echo "found .bashrc"
+    echo "" >> ~/.bashrc
+    echo "# Import grahamgilchrist dotfiles" >> ~/.bashrc
     echo "source ~/.dotfiles/.bash_profile" >> ~/.bashrc
 fi
 
 # Mac OS X uses a .bash_profile for user config
-if [ -f "~/.bash_profile" ]; then
+if [ -f "$HOME/.bash_profile" ]; then
+    echo "found .bash_profile, adding config"
+    echo "" >> ~/.bash_profile
+    echo "# Import grahamgilchrist dotfiles" >> ~/.bash_profile
     echo "source ~/.dotfiles/.bash_profile" >> ~/.bash_profile
 fi
